94b82ebfa7619ec3a636550fc7eddd0e6cbc4e19
ThatLukeDev <null>
2025-04-10 18:31:25 +0100

    memory saved

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-var cloneEnumeratedInnerElement = (element, number) => {
+const REGISTERS = 11;
+const MEMORIES = 99;
+
+const FONTSIZE_SENSITIVITY = 3;
+
+var memoryEventTimeout = null;
+var memory = new Array(REGISTERS + MEMORIES).fill(0);
+var parseByte = (str) => {
+	let num = parseInt(str);
+	if (num < 0) {
+		num = 0;
+	}
+	if (num > 255) {
+		num = 255;
+	}
+	if (num != num) {
+		num = 0;
+	}
+	return num;
+};
+var changeMemoryEvent = () => {
+	document.querySelectorAll(".register>input").forEach((v) => {
+		let num = parseByte(v.value);
+		v.value = num;
+
+		let idx = parseInt(v.outerHTML.replace(/.+?Input(\d+).+/, "$1"));
+		memory[idx] = num;
+	});
+	document.querySelectorAll(".memory>input").forEach((v) => {
+		let num = parseByte(v.value);
+		v.value = num;
+
+		let idx = parseInt(v.outerHTML.replace(/.+?Input(\d+).+/, "$1"));
+		memory[REGISTERS + idx] = num;
+	});
+};
+var cloneEnumeratedInnerElement = (element, number, offset) => {
 	let html = element.innerHTML;
 	element.innerHTML = "";
 	for (let i = 0; i < number; i++) {
 		element.innerHTML += html.replace(/\$_/g, i);
+		element.onchange = () => {
+			clearTimeout(memoryEventTimeout);
+			setTimeout(changeMemoryEvent, memoryEventTimeout);
+		}
 	}
 };
 
 window.onload = () => {
-	cloneEnumeratedInnerElement(document.querySelector(".registers"), 11);
-	cloneEnumeratedInnerElement(document.querySelector(".memoryLocations"), 99);
+	cloneEnumeratedInnerElement(document.querySelector(".registers"), REGISTERS, 0);
+	cloneEnumeratedInnerElement(document.querySelector(".memoryLocations"), MEMORIES, REGISTERS);
 
-	document.querySelectorAll(".register>input").forEach((v) => { console.log(v); v.value = 0; });
-	document.querySelectorAll(".memory>input").forEach((v) => { console.log(v); v.value = 0; });
+	document.querySelectorAll(".register>input").forEach((v) => { v.value = 0; });
+	document.querySelectorAll(".memory>input").forEach((v) => { v.value = 0; });
 
 	let codeInput = document.querySelector("#inputCode");
 
@@ -20,8 +60,8 @@ window.onload = () => {
 	let highlightText = document.querySelector("#highlights");
 
 	fontSliderInput.onchange = () => {
-		fontSliderText.innerHTML = fontSliderInput.value / 3;
-		codeInput.style = `font-size: ${fontSliderInput.value / 3};`
-		highlightText.style = `font-size: ${fontSliderInput.value / 3};`
+		fontSliderText.innerHTML = fontSliderInput.value / FONTSIZE_SENSITIVITY;
+		codeInput.style = `font-size: ${fontSliderInput.value / FONTSIZE_SENSITIVITY};`
+		highlightText.style = `font-size: ${fontSliderInput.value / FONTSIZE_SENSITIVITY};`
 	};
 };
